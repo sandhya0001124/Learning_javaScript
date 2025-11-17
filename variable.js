16fb0d069b43cc938c2a22389e4e7cdc813d755b
Sandhya <arjunsur34@gmail.com>
2025-11-17 19:59:49 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,14 @@
+let name='sandhya'
+let age=22;
+let city='pune';
+
+console.log(name);
+console.log(age);
+console.log(city);
+ city='Latur'
+console.log(city)
+
+const phno=946165
+console.log(phno)
+// phno=654315;
+console.log(phno)
