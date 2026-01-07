d5870113731476b997ced44830b2deb72f3185d4
Sandhya <arjunsur34@gmail.com>
2026-01-07 19:11:14 +0530

    Add files via upload

---
+++ India.js
@@ -0,0 +1,23 @@
+import {a} from "./Russia.js"
+console.log(a);
+
+
+// import demo from "./Russia.js"
+// demo();
+
+import HP from "./Russia.js"
+console.log(HP.crudOil3);
+
+
+
+//? EROOR 
+// let HP="ABC";
+// console.log(HP);
+
+
+import {add} from "./Russia.js"
+console.log(add(10,20));
+
+
+import {sub} from "./Russia.js"
+console.log(sub(30,0));
